398b85d2ed3f4e43559b87123f7aaa9ab1ba3d7d
MartyJ <mr.ershat@gmail.com>
2021-09-27 12:39:56 +0600

    product microservice fixes

--- product-service/src/main/java/kz/iitu/productservice/controller/ProductController.java
+++ product-service/src/main/java/kz/iitu/productservice/controller/ProductController.java
@@ -1,12 +1,11 @@
 package kz.iitu.productservice.controller;
 
+import kz.iitu.productservice.model.Product;
 import kz.iitu.productservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
@@ -24,5 +23,21 @@ public class ProductController {
     public ResponseEntity<?> getProductInformationByName(@PathVariable String name) {
 
         return ResponseEntity.ok(productService.getProductInformationByName(name));
+    }
+    @PostMapping("")
+    public ResponseEntity<?> createProduct(@PathVariable Product product){
+        productService.createProduct(product);
+        return new ResponseEntity<Void>(HttpStatus.CREATED);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Long id){
+        productService.deleteProduct(id);
+        return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateProduct(@PathVariable Long id,@PathVariable Product product){
+        product.setId(id);
+        productService.updateProduct(product);
+        return new ResponseEntity<Void>(HttpStatus.OK);
     }
 }
--- product-service/src/main/java/kz/iitu/productservice/service/impl/ProductServiceImpl.java
+++ product-service/src/main/java/kz/iitu/productservice/service/impl/ProductServiceImpl.java
@@ -1,15 +1,18 @@
 package kz.iitu.productservice.service.impl;
 import kz.iitu.productservice.model.Product;
+import kz.iitu.productservice.repository.ProductRepository;
 import kz.iitu.productservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
+    @Autowired
+    private ProductRepository productRepository;
     @Override
     public Product getProductInformationById(Long id) {
         System.out.println("ProductServiceImpl.getProductInformationById");
@@ -31,5 +34,36 @@ public class ProductServiceImpl implements ProductService {
         product.setDescription("Book description " + name);
         product.setManufacturer(Arrays.asList("Manufacturer #" + name));
         return product;
+    }
+
+    @Override
+    public String findProductNameById(Long id) {
+        return productRepository.findById(id).get().getName();
+    }
+
+    @Override
+    public void createProduct(Product product) {
+        productRepository.saveAndFlush(product);
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        Optional<Product> productOptional = productRepository.findById(id);
+        if(productOptional.isPresent()){
+            productRepository.deleteById(id);
+        }
+    }
+
+    @Override
+    public void updateProduct(Product product) {
+        Optional<Product> productOptional = productRepository.findById(product.getId());
+        if(productOptional.isPresent()){
+            Product product1 = productOptional.get();
+            product.setName(product1.getName());
+            product.setCount(product1.getCount());
+            product.setDescription(product1.getDescription());
+            product.setPrice(product1.getPrice());
+            product.setShopId(product1.getShopId());
+        }
     }
 }
